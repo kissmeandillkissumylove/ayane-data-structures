d0f21fc7decfed41242f539da4203a8d6950689e
kissmeandillkissumylove <kissmeandillkissumylove@gmail.com>
2023-10-05 13:30:54 +0700

    add: test for root

--- ayane_AVL_tree/test_AVL_tree.py
+++ ayane_AVL_tree/test_AVL_tree.py
@@ -3,3 +3,19 @@
 
 import unittest
 import AVL_tree
+
+
+class TestAVLTree(unittest.TestCase):
+	"""class with tests for AVLTree."""
+
+	def setUp(self) -> None:
+		"""create tree."""
+		self.tree = AVL_tree.AVLTree()
+		self.tree.insert([elt for elt in range(0, 100)])
+
+	def test_get_root(self):
+		self.assertIsNotNone(self.tree.get_root())
+
+
+if __name__ == "__main__":
+	unittest.main()
